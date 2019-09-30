98d2fb5fe2290be699e6b8790568b676243736cf
Ubuntu <ubuntu@ip-172-31-25-71.ec2.internal>
2019-09-30 00:12:41 +0000

    pizza program

---
+++ constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Created by: RJ Fromm
+# Created on: Sept 2019
+# This module contains constants for the pizza program
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
